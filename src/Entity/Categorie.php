411e85bc61407ee1ffd3e8c2e0a3f9ccb42fdd68
LY Luxtaing <LY@lan.sio.lyceefulbert.fr>
2023-10-09 14:22:00 +0200

    first commit

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Article::class)]
+    private Collection $LesCategories;
+
+    public function __toString(): string
+    {
+        return $this->libelle;
+    }
+
+    public function __construct()
+    {
+        $this->LesCategories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getLesCategories(): Collection
+    {
+        return $this->LesCategories;
+    }
+
+    public function addLesCategory(Article $lesCategory): static
+    {
+        if (!$this->LesCategories->contains($lesCategory)) {
+            $this->LesCategories->add($lesCategory);
+            $lesCategory->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLesCategory(Article $lesCategory): static
+    {
+        if ($this->LesCategories->removeElement($lesCategory)) {
+            // set the owning side to null (unless already changed)
+            if ($lesCategory->getCategorie() === $this) {
+                $lesCategory->setCategorie(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('contenu',CKEditorType::class, [
+                'config_name' => 'my_custom_config',])
+            ->add('date',DateType::class, [
+                'widget' => 'single_text',
+                ])
+            ->add('user')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
